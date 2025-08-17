5366822d2d21cbb31520fa6444de666266346f82
charbeljawich <charbel.jawich2021@outlook.com>
2025-08-17 14:21:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+
+  //loader sec
+  window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+    setTimeout(() =>{
+      loader.classList.add('hidden');
+    },2500);
+  });
+
+  //toggle icon navbar
+
+  let menuIcon=document.querySelector('#menu-icon');
+  let navBar=document.querySelector('.navbar');
+
+  menuIcon.onclick =() =>{
+    menuIcon.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+  }
+
+  //scroll section
+
+  let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  let top = window.scrollY;
+
+  sections.forEach(sec => {
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document
+          .querySelector('header nav a[href*="' + id + '"]')
+          .classList.add('active');
+      });
+    }
+  });
+
+  // sticky header
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  //remove toglle icon and navbar when click navbar links (scroll)
+
+   menuIcon.classList.remove('bx-x');
+    navBar.classList.remove('active');
+
+};
+
+//contact 
+
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault(); // Prevent page reload
+
+  const formData = {
+    name: form.name.value,
+    email: form.email.value,
+    phone: form.phone.value,
+    subject: form.subject.value,
+    message: form.message.value
+  };
+
+  try {
+    const response = await fetch('https://formspree.io/f/manbzere', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    });
+
+    if (response.ok) {
+      alert("Message sent! I’ll get back to you soon.");
+      form.reset();
+    } else {
+      alert("Oops! Something went wrong.");
+    }
+  } catch (error) {
+    alert("Error sending message. Try again later.");
+  }
+});
+
+
